06b3e50bf091d289c6b8a35d76fd7f5b9adac977
ankit <ankitkuswaha07@gmail.com>
2023-10-24 21:20:49 +0530

    01 printing string and number

---
+++ basics/01 firstprogram.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello akit ";
+    cout<<"\n";
+    cout<<endl;
+    cout<<"hello akit "<<endl;
+    cout<<"hello"<<endl<<"ankit";
+
+}
