c208949af7129506341086a0690dd2155bb94c0c
ankit <ankitkuswaha07@gmail.com>
2023-10-30 18:48:23 +0530

    variables in c++ and modules oprator

--- basics/02 variables and their declaration.cpp
+++ basics/02 variables and their declaration.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<4<<endl;
-    cout<<5<<endl;
-    cout<<4+5;
+int main()
+{
+    int y ;
+    cout<<"Enter a number to return sqaure : ";
+    cin>>y;
+    cout << y * y;
 }
