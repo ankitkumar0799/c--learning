e3fed83ebaf2829f3fe92c7ea5a230218bcf2eb0
ankit <ankitkuswaha07@gmail.com>
2023-11-01 20:26:22 +0530

    divisibleby3or5

---
+++ basics/chapter 2/02 checkthreedigitornot.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int x;
+    // cin >> x;
+    // if (x >= 0)
+    // {
+    //     cout <<x<< endl;
+    // }
+
+    // else if (x < 0 )
+    // {
+    //     cout << -x<< endl;
+    // }
+
+
+
+// program 2 check the seller made profilt or loss
+
+
+
+int p;
+cout<<"enter the cost of a product : ";
+cin>>p;
+
+if (p%5 == 0 && p%3 == 0)
+{
+    cout<<"yes";
+}else{
+    cout<<"no ";
+}
+
+
+
+
+    return 0;
+}
---
+++ basics/chapter 2/03 divisibleby3or5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int x;
+    // cin >> x;
+    // if (x >= 0)
+    // {
+    //     cout <<x<< endl;
+    // }
+
+    // else if (x < 0 )
+    // {
+    //     cout << -x<< endl;
+    // }
+
+
+
+// program 2 check the seller made profilt or loss
+
+
+
+int a,b,c;
+cout<<"enter the side of triangle : ";
+cin>>a>>b>>c;
+
+if (p%5 == 0 or p%3 == 0)
+{
+    cout<<"yes";
+}else{
+    cout<<"no ";
+}
+
+
+
+
+    return 0;
+}
