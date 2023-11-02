b38d33361d1edb70e62df80de8ade23cd9e9084f
ankit <ankitkuswaha07@gmail.com>
2023-11-02 23:07:24 +0530

    variables

---
+++ advance/01 Keywords and Identifiers/02 Assignment of Keywords and identifiers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter the 1st number";
+    cin>>num1;
+    cout<<"enter the 2st number";
+    cin>>num2;
+    int sum = num1+num2
+    cout<<"THE SUM OF "<<num1<<" and "<<num2<<" is "<<sum;
+
+
+
+
+
+
+    return 0;
+}
