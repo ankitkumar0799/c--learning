e14e466cc6ca0b95bd68e7e5eb57261675c512f3
ankit <ankitkuswaha07@gmail.com>
2023-11-14 15:35:21 +0530

    printNto1 numbers

---
+++ advance/chapter 7 recursion/02 printNto1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void print(int n){
+    if(n==0) return;
+    cout<<n<<endl;
+    print(n-1);
+}
+int main(){
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    print(n);
+    return 0;
+}
